a82a073b8a78504327e881664af7da653f52e08c
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-03-31 20:12:44 +0530

    Check if number of records matches

--- src/test/java/com/Day29/TestAnalyser.java
+++ src/test/java/com/Day29/TestAnalyser.java
@@ -1,14 +1,24 @@
 package com.Day29;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * We have written the test cases here.
  */
 public class TestAnalyser {
+	StateCensusAnalyser analyser;
+
+	@Before
+	public void initialization() {
+		analyser = new StateCensusAnalyser();
+	}
+
 	@Test
-	public void testCensusAnalyser() {
-		StateCensusAnalyser analyser = new StateCensusAnalyser();
+	public void testRecordMatch() {
 		analyser.loadData();
+		assertEquals(true, analyser.checkData());
 	}
 }
